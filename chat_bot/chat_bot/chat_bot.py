aa5ab2a108b169cb8662c8184d89e846bb85d369
lloyd.k(이정환)/kakao <lloyd.k@kakaocorp.com>
2023-09-19 16:57:03 +0900

    버튼 활성화, 아직 콘솔에만 출력가능

--- chat_bot/chat_bot/chat_bot.py
+++ chat_bot/chat_bot/chat_bot.py
@@ -13,18 +13,42 @@ from chat_bot.chatapp import style
 import os
 os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
 
+chat = ChatOpenAI(temperature=0.8)
+system_message = "assistant는 API를 설명하는 챗봇이다."
+system_message_prompt = SystemMessage(content=system_message)
+
+with open("./chat_bot/project_data_카카오싱크.txt", "rt") as f:
+    assistant_template = f.read()
+
+human_template = ("해당 질문에 간단하게 답해주세요\n"
+                  "{qa}\n"
+                  )
+
+
+assistant_message_prompt = HumanMessagePromptTemplate.from_template(assistant_template)
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, assistant_message_prompt, human_message_prompt])
+
+chain = LLMChain(llm=chat, prompt=chat_prompt)
+
 class State(pc.State):
 
     # The current question being asked.
     question: str
 
     # Keep track of the chat history as a list of (question, answer) tuples.
-    chat_history: list[tuple[str, str]]
+    chat_history: str
 
     def answer(self):
         # Our chatbot is not very smart right now...
         answer = "I don't know!"
         self.chat_history.append((self.question, answer))
+
+    def handle_submit(self, form_data):
+        self.question = form_data['qa']
+        ans = chain.run(qa=self.question)
+        self.chat_history += f"question: {self.question} \n answer: {self.answer}"
+        print(ans)
 
 def qa(question: str, answer: str) -> pc.Component:
     return pc.box(
@@ -41,16 +65,9 @@ def qa(question: str, answer: str) -> pc.Component:
 
 
 def chat() -> pc.Component:
-    qa_pairs = [
-        (
-            "What is Reflex?",
-            "A way to build web apps in pure Python!",
-        ),
-        (
-            "What can I make with it?",
-            "Anything from a simple website to a complex web app!",
-        ),
-    ]
+    qa_str = State.chat_history
+    qa_pairs = []
+
     return pc.box(
         *[
             qa(question, answer)
@@ -60,12 +77,16 @@ def chat() -> pc.Component:
 
 
 def action_bar() -> pc.Component:
-    return pc.hstack(
-        pc.input(
-            placeholder="Ask a question",
-            style=style.input_style,
+    return pc.form(
+        pc.hstack(
+            pc.input(
+                placeholder="Ask a question",
+                style=style.input_style,
+                id="qa",
+            ),
+            pc.button("Ask", style=style.button_style, type_="submit"),
         ),
-        pc.button("Ask", style=style.button_style),
+        on_submit=State.handle_submit
     )
 
 
